9aff9c881ca0dd20ee3d328921a19aaea74f83c1
Leonardo Ribeiro De Almeida <Leozinhooalmeida@hotmail.com>
2023-05-12 12:21:16 -0300

    Terceiro Exercicio

---
+++ Terceiro Exercicio(Feito por mim.py
@@ -0,0 +1,13 @@
+
+genero = input("Qual é o seu sexo M ou F ?")
+masculino ='M'
+feminino = 'F'
+
+while genero != masculino or feminino:
+    print('Use F para feminino e M para masculino')
+    genero = input("Qual é o seu sexo M ou F ?")
+    
+print ('ok')  
+
+  
+    
